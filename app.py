b7d5fcaae3b6dbefe209eb3bc232ae10b1d04355
Kamil Dabek <dabekk@tcd.ie>
2025-02-21 14:48:43 +0100

    Adding new joiners Lucas and Nenad.

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ st.set_page_config(
 
 # Constants
 TEAM_MEMBERS = [
-    'Select Name','Eline', 'Josi', 'Kamil', 'Kerstin', "Moritz", 'Steffi', 
-    'Carolin', 'Mieke', 'Niklas', 'Stani', 'Tobias'
+    'Select Name','Eline', 'Josi', 'Kamil', 'Kerstin', 'Lucas', 'Moritz', 'Steffi', 
+    'Carolin', 'Mieke', 'Nenad', 'Niklas', 'Stani', 'Tobias'
 ]
 LOTTIE_URL = "https://lottie.host/6dab44db-ae63-4dd0-aab7-6f5ebd6cbb32/yz2WTpQJFR.json"
 
